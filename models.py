2a5533bb4c12c16cb04539f637d4bcec0868ffc9
Sigourney, Patrick <Patrick.Sigourney@cdk.com>
2021-05-03 13:30:20 -0700

    PCP-1024/Add unique constraints

--- models.py
+++ models.py
@@ -1,59 +1,86 @@
 import uuid
-from django.db.models import Model, UUIDField, CharField, JSONField, ManyToManyField, ForeignKey, CASCADE
+from django.db.models import Model, UUIDField, CharField, JSONField, ManyToManyField, ForeignKey, UniqueConstraint, CASCADE
 
 class Account(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(max_length=75)
-    properties = JSONField()
+    properties = JSONField(default=None)
     def __str__(self):
-        return self.name  
+        return self.name 
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['name'], name='unique_account_name')
+        ]
 
 class Location(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(max_length=50)
     account = ForeignKey(Account, on_delete=CASCADE)
-    properties = JSONField()
+    properties = JSONField(default=None)
     def __str__(self):
         return self.name
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['name'], name='unique_location_name')
+        ]
 
 class Environment(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(max_length=50)
     location = ForeignKey(Location, on_delete=CASCADE)
-    properties = JSONField()
+    properties = JSONField(default=None)
     def __str__(self):
         return self.name
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['name','location'], name='unique_environment_name')
+        ]
 
 class Cluster(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(max_length=50)
     environment = ForeignKey(Environment, on_delete=CASCADE)
-    properties = JSONField()
+    properties = JSONField(default=None)
     def __str__(self):
         return self.name
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['name','environment'], name='unique_cluster_name')
+        ]
 
 class Team(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(max_length=50)
-    properties = JSONField()
+    properties = JSONField(default=None)
     def __str__(self):
         return self.name
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['name'], name='unique_team_name')
+        ]
 
 class Product(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(max_length=50)
     environment = ForeignKey(Environment, on_delete=CASCADE)
-    properties = JSONField()
+    properties = JSONField(default=None)
     def __str__(self):
         return self.environment.name + "-" + self.name
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['name', 'environment'], name='unique_product_name')
+        ]
 
-class Egress(Model):
+class SecurityProvider(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(max_length=100)
-    asset = ForeignKey(Asset, on_delete=CASCADE)
-    properties = JSONField()
+    properties = JSONField(default=None)
     def __str__(self):
-        return self.asset.name + ":" + self.name
+        return self.name
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['name'], name='unique_security_name')
+        ]
 
 class Asset(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -63,21 +90,37 @@ class Asset(Model):
     clusters = ManyToManyField(Cluster)
     team = ForeignKey(Team, on_delete=CASCADE)
     product = ForeignKey(Product, on_delete=CASCADE)
-    securities =  ManyToManyField(Security)
-    properties = JSONField()
+    securities =  ManyToManyField(SecurityProvider)
+    properties = JSONField(default=None)
     def __str__(self):
         return self.environment.name + "-" + self.asset_id
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['asset_id', 'environment'], name='unique_asset_assetid'),
+            UniqueConstraint(fields=['name', 'environment'], name='unique_asset_name')
+        ]
 
-class Security(Model):
+class EgressRule(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(max_length=100)
-    properties = JSONField()
+    asset = ForeignKey(Asset, on_delete=CASCADE)
+    properties = JSONField(default=None)
     def __str__(self):
-        return self.name
+        return self.asset.name + ":" + self.name
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['name', 'asset'], name='unique_egress_name')
+        ]
+
 
 class Resource(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(max_length=100)
-    properties = JSONField()
+    environment = ForeignKey(Environment, on_delete=CASCADE)
+    properties = JSONField(default=None)
     def __str__(self):
         return self.name
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['name', 'environment'], name='unique_resource_name')
+        ]
