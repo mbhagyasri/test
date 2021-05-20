8437d628273adc1a1beedcc58f6b62167339d30a
Mai Le <Mai.Le@cdk.com>
2021-05-20 12:52:07 -0700

    add migrate as part of entrypoint.sh

--- athena_app_cmdb/scripts/entrypoint.sh
+++ athena_app_cmdb/scripts/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 python manage.py flush --no-input
+python manage.py makemigrations athena_app_cmdb
+python manage.py migrate
 gunicorn --config /opt/athena_app_cmdb/gunicorn_cfg.py app_registry.wsgi:application
+
 exec "$@"
