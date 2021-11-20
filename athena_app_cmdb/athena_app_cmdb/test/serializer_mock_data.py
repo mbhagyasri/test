47ddc7500f5923abf63ab26790b1557c6d723567
D'louhy, Jesse <Jesse.Dlouhy@cdk.com>
2021-11-19 16:12:30 -0800

    Add mock data

--- athena_app_cmdb/athena_app_cmdb/test/serializer_mock_data.py
+++ athena_app_cmdb/athena_app_cmdb/test/serializer_mock_data.py
@@ -50,3 +50,74 @@ mockasset =  {
         "description": "example test",
         "assetMasterId": 500
     }
+
+mockteam = {
+    "id": "team-example-team",
+    "name": "example-team",
+    "deleted": False,
+    "_type": "team",
+    "email": [
+        "AthenaPlatformDevelopers@cdk.com"
+    ],
+    "ad-group": [
+        "Athena Platform Developers",
+        "Athena Platform Developers (DSI)"
+    ],
+    "opsgenie": {
+        "name": "athena-platform",
+        "create": True,
+        "manager": "athenaplatformdevelopers@cdk.com"
+    },
+    "description": "Athena Platform Team",
+    "notification": [
+        {
+            "email": "AthenaPlatformDevelopers@cdk.com"
+        },
+        {
+            "slack": "https://cdk-global.slack.com/archives/GPCG44ZPU"
+        }
+    ]
+}
+
+mockproduct = {
+    "id": "example-product",
+    "environments": [
+        {
+            "id": "us-dev",
+            "type": "dev",
+            "prefix": "dev",
+            "location": "location-us-dev"
+        },
+        {
+            "id": "us-prod",
+            "type": "prod",
+            "location": "location-us-prod"
+        },
+        {
+            "id": "us-stage",
+            "type": "stage",
+            "prefix": "stage",
+            "location": "location-us-nonprod"
+        }
+    ],
+    "deleted": False,
+    "security": [
+        {
+            "provider": "fortellis",
+            "configuration": {}
+        }
+    ],
+    "external-ids": {
+        "iqr_product_id": 1234
+    },
+    "bamboo-projects": [
+        {
+            "id": "NGIP"
+        }
+    ],
+    "bitbucket-projects": [
+        {
+            "id": "ATHENAP"
+        }
+    ]
+}
