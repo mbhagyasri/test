3c550addc635177e470f26a0514b27b56001dc14
Cat-Tuong Tu <cat-tuong.tu@cdk.com>
2021-11-30 11:30:43 -0800

    Added tests for the models: DatabaseChangeLog, DatabaseChangeLogLock, EnvType, LocationRegion, LocationStatus, Team, Product, and ProductEnvironment.

--- athena_app_cmdb/athena_app_cmdb/test/test_models.py
+++ athena_app_cmdb/athena_app_cmdb/test/test_models.py
@@ -4,19 +4,8 @@
 # Copyright (c) 2021 =================================================
 
 from django.test import TestCase
-from athena_app_cmdb.models import Location, AppLanguage, AssetType, Cluster
+from athena_app_cmdb.models import Location, AppLanguage, AssetType, Cluster, DatabaseChangeLog, DatabaseChangeLogLock, EnvType, LocationRegion, LocationStatus, Team, Product, ProductEnvironment
 
-class LocationModelTests(TestCase):
-    def setUp(self):
-        Location.objects.create(refid="us-west-2", name="Oregon")
-
-    def test_location_exist(self):
-        location = Location.objects.get(refid="us-west-2")
-        self.assertEqual(location.name, "Oregon")
-
-    def test_string_representation(self):
-        location = Location(refid="us-west-2", name="Oregon")
-        self.assertEqual(str(location), location.refid)
 
 class AppLanguageTests(TestCase):
     def setUp(self):
@@ -60,7 +49,119 @@ class ClusterTests(TestCase):
         cluster = Cluster(id="11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000", refid="cluster_2", uri="example-cluster-uri.com/api")
         link = "/clusters/11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000"
         self.assertEqual(cluster.self_links, link)
+    
+class DatabaseChangeLogModelTests(TestCase):
+    def setUp(self):
+        DatabaseChangeLog.objects.create(id=1, author="tuc", filename="metrics-changelog", orderexecuted=3)
+    
+    def test_database_changelog_exists(self):
+        database_changelog = DatabaseChangeLog.objects.get(id=1)
+        self.assertEqual(database_changelog.author, "tuc")
+    
+class DatabaseChangeLogLockModelTests(TestCase):
+    def setUp(self):
+        DatabaseChangeLogLock.objects.create(id=2, locked=False)
 
+    def test_database_changelog_lock_exists(self):
+        database_changelog_lock = DatabaseChangeLogLock.objects.get(id=2)
+        self.assertEqual(database_changelog_lock.locked, False)
+
+    def test_need_merge_properties(self):
+        database_changelog_lock = DatabaseChangeLogLock(id=2, locked=False)
+        function_value = database_changelog_lock.need_merge_properties
+        message = "Test value is not false."
+        self.assertFalse(function_value, message)
+    
+class EnvTypeModelTests(TestCase):
+    def setUp(self):
+        EnvType.objects.create(id=10, name="prod", deleted='f')
+    
+    def test_env_type_exists(self):
+        env_type = EnvType.objects.get(id=10)
+        self.assertEqual(env_type.name, "prod")
+    
+    def test_string_representation(self):
+        env_type = EnvType.objects.get(id=10)
+        self.assertEqual(str(env_type), "prod")
+
+class LocationRegionModelTests(TestCase):
+    def setUp(self):
+        LocationRegion.objects.create(id=7, name="us-west", deleted='f')
+    
+    def test_location_region_exists(self):
+        location_region = LocationRegion.objects.get(id=7)
+        self.assertEqual(location_region.name, "us-west")
+    
+    def test_string_representation(self):
+        location_region = LocationRegion.objects.get(id=7)
+        self.assertEqual(str(location_region), "us-west")
+
+class LocationStatusModelTests(TestCase):
+    def setUp(self):
+        LocationStatus.objects.create(id=5, name="online", deleted='t')
+
+    def test_location_status_exists(self):
+        location_status = LocationStatus.objects.get(id=5)
+        self.assertEqual(location_status.name, "online")
+    
+    def test_string_representation(self):
+        location_status = LocationStatus.objects.get(id=5)
+        self.assertEqual(str(location_status), "online")
+
+class LocationModelTests(TestCase):
+    def setUp(self):
+        Location.objects.create(refid="us-west-2", name="Oregon")
+
+    def test_location_exist(self):
+        location = Location.objects.get(refid="us-west-2")
+        self.assertEqual(location.name, "Oregon")
+
+    def test_string_representation(self):
+        location = Location(refid="us-west-2", name="Oregon")
+        self.assertEqual(str(location), location.refid)
+
+class TeamModelTests(TestCase):
+    def setUp(self):
+        Team.objects.create(id="11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000", refid="team-10", name="fortellis")
+    
+    def test_team_exists(self):
+        team = Team.objects.get(refid="team-10")
+        self.assertEqual(team.name, "fortellis")
+
+    def test_self_links(self):
+        team = Team.objects.get(refid="team-10")
+        links = "/teams/11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000"
+        self.assertEqual(team.self_links, links)
+    
+    def test_string_representation(self):
+        team = Team.objects.get(name="fortellis")
+        self.assertEqual(str(team), team.refid)
+
+class ProductModelTests(TestCase):
+    def setUp(self):
+        Product.objects.create(id="11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000", refid="dms")
+
+    def test_product_exists(self):
+        product = Product.objects.get(id="11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000")
+        self.assertEqual(product.refid, "dms")
+    
+    def test_string_representation(self):
+        product = Product.objects.get(id="11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000")
+        self.assertEqual(str(product), product.refid)
+
+class ProductEnvironmentModelTests(TestCase):
+    def setUp(self):
+        Location.objects.create(refid="us-west-2", name="Oregon")
+        EnvType.objects.create(id=10, name="prod", deleted='f')
+        ProductEnvironment.objects.create(id="11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000", refid="dev", location=Location.objects.get(refid="us-west-2"), type=EnvType.objects.get(id=10))
+
+    def test_product_exists(self):
+        product_environment = ProductEnvironment.objects.get(id="11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000")
+        self.assertEqual(product_environment.refid, "dev")
+    
+    def test_string_representation(self):
+        product_environment = ProductEnvironment.objects.get(id="11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000")
+        self.assertEqual(str(product_environment), product_environment.refid)
 
     
 
